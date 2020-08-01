11da9c5b21014609dd315d8c30b60099891398b4
Gwonsoo Lee <gslee@bighitcorp.com>
2020-08-02 00:58:54 +0900

    fix checking date logic

---
+++ pkg/aws/cloudwatch_test.go
@@ -0,0 +1,34 @@
+package aws
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCheckMetricTimeValidation(t *testing.T) {
+	baseTime := time.Now()
+	testData := []map[string]interface{}{
+		{
+			"start": baseTime.Add(-1 * time.Second),
+			"end": baseTime.Add(1 * time.Second),
+			"expected": true,
+		},
+		{
+			"start": baseTime.Add(-1 * time.Second),
+			"end": baseTime.Add(-2 * time.Second),
+			"expected": false,
+		},
+		{
+			"start": baseTime.Add(1 * time.Second),
+			"end": baseTime.Add(1 * time.Second),
+			"expected": false,
+		},
+	}
+
+	for _, td := range testData {
+		if CheckMetricTimeValidation(td["start"].(time.Time), td["end"].(time.Time)) != td["expected"].(bool) {
+			t.Error("CheckMetricTimeValidation failed")
+		}
+	}
+
+}
